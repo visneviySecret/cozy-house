b7f37dc28fe3e1552837449e2b5b90f8942c05ba
visneviySecret <visneviy.secret@yandex.ru>
2023-06-27 18:46:11 +0300

    fix visual bag

--- src/Features/NavMenu.vue
+++ src/Features/NavMenu.vue
@@ -88,11 +88,11 @@ const links = [
 .menu {
     display: block;
     position: absolute;
-    width: 100%;
     top: 50%;
     left: 50%;
     display: flex;
     flex-direction: column;
+    text-wrap: nowrap;
     gap: 43px;
 
     @media (max-width: $tablet) {
