49393e1fe7710620d9430a823ca12fbd3768ba62
visneviySecret <visneviy.secret@yandex.ru>
2023-06-27 14:50:45 +0300

    fix branch name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:visneviySecret/cozy-house.git main:gh-pages
+git push -f git@github.com:visneviySecret/cozy-house.git master:gh-pages
 cd -
